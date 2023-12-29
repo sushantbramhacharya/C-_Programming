b50e343b856919393c3f363fa0fdf306d2899194
sushantbramhacharya <sushantbramacharya@gmail.com>
2023-12-29 17:00:27 +0545

    Constructor Moved Above

--- OOPS_in_C#/Enum.cs
+++ OOPS_in_C#/Enum.cs
@@ -8,6 +8,29 @@ namespace OOPS_in_C_
 {
     internal class Enum
     {
+        internal Enum()
+        {
+            double x = 5;
+            double y = 4;
+
+            double answer;
+            //Manipulating Values outside the method.Using out keyword
+            //Passing Reference of the var answer
+            //Out doesnt need to be initilized
+            GetSum(out answer, x, y);
+            Console.WriteLine("The answer of the calculation is " + answer);
+
+            //Pass by refernce using ref keyword
+            int a = 10, b = 20;
+            Console.WriteLine("Before Swap a is {0} b is {1}", a, b);
+            SwapInt(ref a, ref b);
+            Console.WriteLine("After Swap a is {0} b is {1}", a, b);
+
+            //Params keyword
+            Console.WriteLine("Get Sum more {0} + {1} + {2} = {3}", a, b, x, GetSumMore(a, b, x));
+
+            PrintInfo(zipCode: 15167, name: "Sus");
+        }
         static void GetSum(out double answer,double x = 1, double y = 1)
         {
             answer= x+y;
@@ -19,28 +42,7 @@ namespace OOPS_in_C_
             num1 = num2;
             num2 = temp;
         }
-        internal Enum() {
-            double x = 5;
-            double y = 4;
-
-            double answer;
-            //Manipulating Values outside the method.Using out keyword
-            //Passing Reference of the var answer
-            //Out doesnt need to be initilized
-            GetSum(out answer,x, y);
-            Console.WriteLine("The answer of the calculation is "+answer);
-
-            //Pass by refernce using ref keyword
-            int a=10, b=20;
-            Console.WriteLine("Before Swap a is {0} b is {1}",a,b);
-            SwapInt(ref a,ref b);
-            Console.WriteLine("After Swap a is {0} b is {1}", a, b);
-
-            //Params keyword
-            Console.WriteLine("Get Sum more {0} + {1} + {2} = {3}", a, b,x, GetSumMore(a,b,x));
-
-            PrintInfo(zipCode: 15167, name: "Sus");
-        }
+        
         static double GetSumMore(params double[] nums) 
         { 
             double answer=0;
